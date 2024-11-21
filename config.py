8efbd9cbf536aaca009b60bb135959c14bba0581
SHIVAM MALVIYA <145798546+shivuu2005@users.noreply.github.com>
2024-11-21 13:55:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,5 @@ import os
 
 class Config:
     SECRET_KEY = 'Shivam2005'  # Secret key for sessions
-    SQLALCHEMY_DATABASE_URI = postgresql://rgpv_bot_db_user:Jea6fFA5qdnWLDL8cRbdMUHdlUZMQOsm@dpg-csl2usa3esus73fvles0-a/rgpv_bot_db
-    
-    
+    SQLALCHEMY_DATABASE_URI = 'postgresql://rgpv_bot_db_user:Jea6fFA5qdnWLDL8cRbdMUHdlUZMQOsm@dpg-csl2usa3esus73fvles0-a/rgpv_bot_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Disable track modifications for performance reasons
